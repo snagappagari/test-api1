3e9154db7acddbdeac2658a408dbfa41bd39b678
Raj Kumar <64770654+rajthummeda@users.noreply.github.com>
2025-04-28 15:44:14 +0530

    update

---
+++ src/test/java/com/otsi/test_api/TestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otsi.test_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
